7fcfafe543d684c4ee09b9565fe2bc2bf8da3fee
Stephen von Takach <steve@advancedcontrol.com.au>
2013-03-05 09:16:22 +1100

    removed iconv dep

--- app/models/auth_source_ldap.rb
+++ app/models/auth_source_ldap.rb
@@ -16,7 +16,7 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 require 'net/ldap'
-require 'iconv'
+#require 'iconv'
 require 'base64'
 
 class AuthSourceLdap < AuthSource
